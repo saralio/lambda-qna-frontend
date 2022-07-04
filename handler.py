c9f8da5c967bf1cbe7225fdcb74c961f63ba0b78
mohit2152sharma <mohitlakshya@gmail.com>
2022-07-05 00:43:40 +0530

    adding template for qna

--- handler.py
+++ handler.py
@@ -1,10 +1,12 @@
+import urllib
 from jinja2 import Environment, FileSystemLoader
 from saral_utils.extractor.dynamo import DynamoDB
 from saral_utils.utils.env import get_env_var
 from utils import normalize_links, normalize_options, links_exist, opt_markdown, qna_markdown
 
+
 def send_qna(event, context):
-    
+
     # get the question id
     question_id = event['pathParameters']['question_id']
     print(f'Requested Question Id: {question_id}')
@@ -24,15 +26,20 @@ def send_qna(event, context):
         links = normalize_links(links=question['links']['L'])
     else:
         links = None
-    correct_option_text = 'Uh-oh! Your answer is incorrect. Correct answer is: ' + [opt['text'] for opt in options if opt['is_correct']][0]
+    correct_option_text = 'Uh-oh! Your answer is incorrect. Correct answer is: ' + [
+        opt['text'] for opt in options if opt['is_correct']][0]
+
+    tweet_text = f"Check out this question by @data_question on #RStats: https://g2kzt52kkb.execute-api.ap-south-1.amazonaws.com/test/qna/{question_id}\nYou can subscribe at http://saral.club for more such questions."
+    tweet_encode = urllib.parse.quote_plus(tweet_text)   # type:ignore
 
     template_data = {
-        'question_text': question_text, 
-        'answer_text': explaination_text, 
-        'options': options, 
-        'links': links, 
+        'question_text': question_text,
+        'answer_text': explaination_text,
+        'options': options,
+        'links': links,
         'has_links': has_links,
-        'correct_option_text': correct_option_text
+        'correct_option_text': correct_option_text,
+        'tweet':tweet_encode
     }
 
     env = Environment(loader=FileSystemLoader('.'))
@@ -41,10 +48,10 @@ def send_qna(event, context):
 
     with open('./templates/qna-template.html', 'r') as file:
         html_template = file.read()
-    
+
     template = env.from_string(html_template)
     content = template.render(question=template_data)
-    
+
     response = {
         "statusCode": 200,
         "body": content,
@@ -56,4 +63,4 @@ def send_qna(event, context):
         }
     }
 
-    return response
+    return response 
